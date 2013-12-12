e346b821ef62f8bb3750d8d1213b2f2ed5e601ea
Anthony Tiradani <tiradani@fnal.gov>
2013-12-12 13:34:10 -0600

    removed unneeded and unused files

--- manifests/context.pp
+++
@@ -1,5 +0,0 @@
-define limits::file ( $config = undef, ) {
-  $limits_context = {'limits_context' => $title }
-  $merged_config = merge($config, $limits_context)
-  create_resources( 'limits::domain', $merged_config )
-}
